d4cda77d10e92c9f4c4cba62ccf9f4aa6138c57c
MrMarvel2099 <ahmedmaher17296@gmail.com>
2023-05-20 09:56:36 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Setting Up
+let menuToggle = document.querySelector('.menuToggle')
+let rotMenu = document.querySelector('.rotMenu')
+
+// Portfolio Toggle
+menuToggle.onclick = function(){
+    rotMenu.classList.toggle('active')
+}
